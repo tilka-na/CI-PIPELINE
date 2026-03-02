3f87463bd74c2e5c7e81be0ba8d71b6ea78e8513
kawtar <kawtarelouazzani2006@gmail.com>
2026-03-02 14:24:42 +0000

    correction dans user_manager.py

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,23 +1,23 @@
 class UserManager:
     def __init__(self):
         self.users = []
-    
+
     def add(self, username):
-        if not username:    
+        if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire")
         if username in self.users:
             raise ValueError("Utilisateur deja existant")
         self.users.append(username)
-    
-    def remove_user(self, username):    
+
+    def remove_user(self, username):
         if username not in self.users:
             raise ValueError("Utilisateur introuvable")
         self.users.remove(username)
-    
+
     def count_users(self):
         return len(self.users)
-    
+
     def count_total_users(users):
-        #tmp=0
+        # tmp=0
         return len(users)
-    ##ajout de commentaire
+    # ajout de commentaire
